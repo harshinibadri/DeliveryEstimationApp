b3998e216c17995bff931902a74d0f2216586773
harshinibadri <148305218+harshinibadri@users.noreply.github.com>
2024-11-10 20:03:00 +0530

    Update app.js

--- delivery-backend/app.js
+++ delivery-backend/app.js
@@ -6,7 +6,8 @@ const Pincode = require('./models/Pincode');
 const { getAllDocuments } = require('./utils/dbUtils');
 const app = express();
 app.use(cors({ origin: ['https://delivery-estimation-website.vercel.app']
-             methods:["GET"}));
+             methods:["GET"],
+              credentials: true}));
 app.use(express.json());
 // Function to get delivery estimate
 const getDeliveryEstimate = (provider, pincode, orderTime, inStock) => {
